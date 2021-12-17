b87783ccda08d55e9dc757b69a05f69f719ea952
alanmdf <alanmoreirascr@gmail.com>
2021-12-17 15:34:17 -0300

    Updates

---
+++ precache-manifest.51449f6fe86af942c954747be9b5e76e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9c01251cb67f64bfbe5b86999fecb29",
+    "url": "/trybewallet/index.html"
+  },
+  {
+    "revision": "65e3723b7c1e49b38787",
+    "url": "/trybewallet/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d53e2da2f76571ca3d02",
+    "url": "/trybewallet/static/js/2.638f0f01.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybewallet/static/js/2.638f0f01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65e3723b7c1e49b38787",
+    "url": "/trybewallet/static/js/main.8ac35536.chunk.js"
+  },
+  {
+    "revision": "5dc72ebdcdec490a7442",
+    "url": "/trybewallet/static/js/runtime-main.ec83dc77.js"
+  }
+]);
